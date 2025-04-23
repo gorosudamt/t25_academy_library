2279b2dc72d8cb5ccce9e5da3754fb9d89597623
gorosudamt <goro.suda@metateam.co.jp>
2025-04-23 09:59:58 +0900

    書籍登録です

--- src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
+++ src/main/java/jp/co/metateam/library/repository/BookMstRepository.java
@@ -2,8 +2,14 @@ package jp.co.metateam.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import jp.co.metateam.library.model.Account;
 import jp.co.metateam.library.model.BookMst;
+import jp.co.metateam.library.model.BookMstDto;
+import jp.co.metateam.library.service.BookMstService;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,7 +17,19 @@ public interface BookMstRepository extends JpaRepository<BookMst, Long> {
 
 	@Query(value = "SELECT * FROM book_mst LIMIT 1000", nativeQuery = true)
 	List<BookMst> findLimitedBook();
+	
 
 	@Query(value = "SELECT * FROM book_mst WHERE id = ?1", nativeQuery = true)
 	Optional<BookMst> selectById(Long id);
+
+@Query("SELECT b FROM BookMst b WHERE b.isbn = :isbn")
+List<BookMst> findByIsbn(@Param("isbn") String isbn);
+
+
+	@Query(value = "SELECT * FROM book_mst ", nativeQuery = true)
+	Optional<BookMst> selectByTitle(String title);
+	
 }
+
+
+
